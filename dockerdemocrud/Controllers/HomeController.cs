ca73b62380adc3e9f057047c0c7359a343451f2e
Rahul Bavaliya <rsbavaliya330@gmail.com>
2022-06-01 08:21:19 +0530

    .net demo in github

---
+++ dockerdemocrud/Controllers/HomeController.cs
@@ -0,0 +1,146 @@
+﻿using dockerdemocrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dockerdemocrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        List<Movie> movies = new List<Movie>();
+        public IActionResult Index()
+        {
+            //context.Movie.ToList();
+            var m = "a";//we need to return list of movie 
+
+            for(int i = 0; i < 25; i++)
+            {
+                Movie movie = new Movie();
+                movie.Id = i.ToString()+1;
+                movie.Name = "ABCD";
+                movie.Actors = "Don't know";
+                movies.Add(movie);
+            }
+            
+
+
+
+            var m1 = movies.ToList();
+
+            return View(m1);
+        }
+
+        [HttpGet]
+        public List<Movie> getMovieList()
+        {
+            var m = "a";//we need to return list of movie 
+
+            for (int i = 0; i < 25; i++)
+            {
+                Movie movie = new Movie();
+                movie.Id = i.ToString() + 1;
+                movie.Name = "ABCD"+i;
+                movie.Actors = "Don't know"+i;
+                movies.Add(movie);
+            }
+            return movies;
+        }
+
+
+        [HttpPost]
+        public List<Movie> sendData([FromBody()] Movie movie1)
+        {
+            var m = movie1;//we need to return list of movie 
+            for (int i = 0; i < 25; i++)
+            {
+                Movie movie = new Movie();
+                movie.Id = i.ToString() + 1;
+                movie.Name = "ABCD" + i;
+                movie.Actors = "Don't know" + i;
+                movies.Add(movie);
+            }
+
+            movies.Add(movie1);
+            return movies;
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+       
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                // we need to pass as a parameter as movie object m
+                //Add new data into db using sp or qeury
+                movies.Add(m);
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+       
+        public IActionResult Update(int id)
+        {
+            //var pc = context.Movie.Where(a => a.Id == id).FirstOrDefault();
+            var pc = "sdf";//we need to update movie object data in db 
+            return View(pc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var pc = "sdf";//we need to update movie object data in db 
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View(m);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //var pc = context.Movie.Where(a => a.Id == id).FirstOrDefault();
+            //context.Remove(pc);
+            //await context.SaveChangesAsync();
+
+            //we need to delete data from db where id 
+
+            return RedirectToAction("Index");
+        }
+    }
+}
